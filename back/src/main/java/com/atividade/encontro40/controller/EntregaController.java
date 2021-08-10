beddbbed8cf19713d0108880feb8ec73e81e40ce
Vander <vstecnologia@gmail.com>
2021-08-10 10:52:10 -0400

    Liberando CORS

--- back/src/main/java/com/atividade/encontro40/controller/EntregaController.java
+++ back/src/main/java/com/atividade/encontro40/controller/EntregaController.java
@@ -3,7 +3,9 @@ package com.atividade.encontro40.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ import com.atividade.encontro40.repository.EntregaRepositorio;
 
 @RestController
 @RequestMapping("entregas")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8090", "http://localhost:9876"} )
 public class EntregaController {
 	
 	@Autowired
@@ -23,7 +26,7 @@ public class EntregaController {
 		return repositorio.findAll();
 	}
 	
-	@GetMapping("incluir")
+	@PostMapping("incluir")
 	public void incluir(@RequestBody Entrega novaEntrega) {
 		repositorio.save(novaEntrega);
 		repositorio.flush();
